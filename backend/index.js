c1681d496f944b4fcf86ba0fcc8ba8677a89c25b
tufan özkan <tuf.ozkan@gmail.com>
2024-04-22 11:38:29 +0300

    models created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-// Define a route
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+
+mongoose.set('strictQuery', false);
+const key =
+	'mongodb+srv://theozkan1905:twofun1905@cluster0.np0ed2c.mongodb.net/yazlab2?retryWrites=true&w=majority';
+main().catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let usersRouter = require('./routes/UsersRouter');
+app.use('/users', usersRouter);
+
+app.use(function (req, res, next) {
+	next(createError(404, 'Not Found'));
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+// Error handling
+app.use(function (err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	res.status(err.status || 500);
+	res.send('error');
 });
+
+async function main() {
+	await mongoose.connect(key);
+}
+
+module.exports = app;
---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+// User.js dosyası
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+const Users = mongoose.model('Users', usersSchema);
+
+module.exports = Users;
